ea6a21fcbde7adff89babb31eb5be44109edbd05
anso-home <juan.anso.research@gmail.com>
2025-08-21 21:44:52 +0200

    Create magicians.py
    Chapter 4. Working with lists

---
+++ magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    # print(magician)
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see the next trick {magician.title()}.\n")
+
+print("Thanks everyone, that was a great show!.\n")
